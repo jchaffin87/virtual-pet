cbaa6498897c0b3abfb8920285763adb38425fe3
John <jchaffin87@gmail.com>
2017-10-06 22:39:33 -0400

    just getting started. pushing initial work.

--- src/VirtualPet.java
+++ src/VirtualPet.java
@@ -1,4 +1,18 @@
 
 public class VirtualPet {
 
+	public String name;
+	private int hunger;
+	private int thirst;
+	private int boredom;
+	private int fatigue;
+	
+	public VirtualPet(int hunger, int thirst, int boredom, int fatigue){
+	 this.hunger = hunger;
+	 this.thirst = thirst; 
+	 this.boredom = boredom; 
+	 this.fatigue = fatigue;
+	}
+	
+	
 }
--- src/VirtualPetApp.java
+++ src/VirtualPetApp.java
@@ -1,8 +1,15 @@
-
+import java.util.Scanner;
 public class VirtualPetApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Scanner input = new Scanner(System.in);
+		VirtualPet dog1 = new VirtualPet(0,0,0,0);
+		
+		System.out.println("Welcome to VirtualPet! Meet your new dog, Fido!");
+		System.out.println("Fido: Woof! Woof!");
+		System.out.println("It's your job to take care of Fido. \nMake sure he doesn't get too hungry, thirsty, bored, or tired.");
+		
 
 	}
 
